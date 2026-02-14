6ba6203e8d24700f1d86c71d4e29231eeb94e602
akcoding <suryaputrak764@gmail.com>
2026-02-14 21:23:26 +0530

    app.py changes

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ def view_playlist(playlist_id):
 # ------------------ RUN APP ------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
